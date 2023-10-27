1940ac6dbb78ccfc086795fd207efad5578c4d52
“Matheus” <moisesai@Matheuss-MacBook-Air.local>
2023-10-27 17:24:58 -0300

    init

---
+++ start-app/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  
+
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Ola</h1>
+      <h1>Oi</h1>
+      <h1>Teste</h1>
+    </main>
+  )
+}
